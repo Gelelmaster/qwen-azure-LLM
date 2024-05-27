646f550b084dc32cfc3b2b4f46f5d9553f002ff0
Gary <tel3023007033@gmail.com>
2024-05-27 16:19:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from dashscope import Generation
-from dashscope.api_entities.dashscope_response import Role
 from recognize_speech import recognize_speech
 from text_to_speech import text_to_speech
 from play_audio import play_audio
 # 浏览器相关调用
-from website_mapping import keyword_to_url  # 导入外部文件中的字典
-from website_mapping import website_keyword
-from website_mapping import software_keyword
-from website_mapping import software_mapping
+from dict import keyword_to_url  # 导入外部文件中的字典
+from dict import website_keyword
+from dict import software_keyword
+from dict import software_mapping
+from model import model
 import webbrowser
 # 打开程序等系统调用
 import os
 
 # 判断是否退出函数
 def should_exit(message):
-     return message.lower() in {'结束','你退下吧','退下','退下吧'}
+     return message.lower() in {'结束','你退下吧','退下','退下吧','好的你退下吧',}
+
 # 打开网站函数
 def open_website(message):
     url = keyword_to_url.get(message.lower(), None)
@@ -23,6 +23,7 @@ def open_website(message):
         webbrowser.open(url)
     else:
         print("未知的关键词，请输入有效的网站关键词。")
+
 # 打开程序函数
 def open_software(message):
     command = software_mapping.get(message.lower(), None)
@@ -41,7 +42,7 @@ def main():
             
             # 判断退出指令
             if should_exit(message):
-                text_to_speech('好的，如果你需要任何帮助或有其他问题，请随时告诉我。祝你有美好的一天！')
+                text_to_speech('好的。祝你有美好的一天！')
                 play_audio("output.wav")
                 print("对话结束，程序即将退出。")
                 break
@@ -65,20 +66,11 @@ def main():
                 play_audio("output.wav")
                 continue
             
-            # 如果以上都不是，调用AI回复
-            whole_message = ''
-            
-            # 调用通义千问大模型，接收流式输出
-            responses = Generation.call(Generation.Models.qwen_plus, messages=messages, result_format='message', stream=True, incremental_output=True)
-            print('通义千问:', end='')
-            for response in responses:
-                content = response.output.choices[0]['message']['content'] if response.output and response.output.choices else ""
-                whole_message += content
-                print(content, end='')
-            print()
-            # 把流式输出的所有消息合成一条输出
-            messages.append({'role': 'assistant', 'content': whole_message})
-            text_to_speech(whole_message)
+            # 如果以上都不是，调用大模型回复
+            model_message = model(messages)
+
+            messages.append({'role': 'assistant', 'content': model_message})
+            text_to_speech(model_message)
             play_audio("output.wav")
     except Exception as e:
         print(f"发生错误: {e}")
