09678093a2b9c47d7c5d8389a4a6915349dab0e2
Gary <tel3023007033@gmail.com>
2024-05-30 13:34:06 +0300

    Delete play_audio.py

--- play_audio.py
+++
@@ -1,8 +0,0 @@
-import sounddevice as sd
-import soundfile as sf
-def play_audio(file_path):
-    # 使用soundfile加载音频文件
-    data, fs = sf.read(file_path, dtype='float32')
-    # 使用sounddevice播放音频
-    sd.play(data, fs)
-    sd.wait()  # 等待音频播放完成
